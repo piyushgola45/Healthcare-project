66f173d7f9fe1f85e4e16b180e75244f98f1236c
piyushgola45 <145822777+piyushgola45@users.noreply.github.com>
2025-04-29 19:41:46 +0530

    Update Wecareforyou.jsx

--- src/components/Wecareforyou.jsx
+++ src/components/Wecareforyou.jsx
@@ -7,8 +7,6 @@ function Wecareforyou()
             </div>
             <br /> <br />
             <div className="row" style ={{'textAlign':'center'}}>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam est atque odit magnam accusamus tenetur mollitia veritatis vitae, culpa aperiam corporis facilis ad unde debitis excepturi earum quia inventore quidem.
-            Atque, debitis rerum? Veritatis ducimus repudiandae asperiores quas numquam. Esse eveniet labore corrupti quaerat autem, voluptatum vitae. Commodi nulla quod tempore ad odio eligendi facilis doloremque eveniet recusandae consectetur! Est!</p>
             </div>
 
         </div>
